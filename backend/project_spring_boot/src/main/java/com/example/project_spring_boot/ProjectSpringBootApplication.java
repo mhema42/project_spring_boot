71c265869cc27e7fc2a30b05713bdf17d1d3ffe3
Mats <mhema@telia.com>
2022-12-05 12:17:53 +0100

    first commit of boilerplate

---
+++ backend/project_spring_boot/src/main/java/com/example/project_spring_boot/ProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ backend/project_spring_boot/src/test/java/com/example/project_spring_boot/ProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
