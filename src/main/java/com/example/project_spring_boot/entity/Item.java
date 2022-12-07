5f464d7ffa8cae1a09a376d06b787252190be5f0
Nordwall <tobiasnordwall@gmail.com>
2022-12-07 15:32:57 +0100

    i have added an enitity, service, controller and repository for item

---
+++ src/main/java/com/example/project_spring_boot/entity/Item.java
@@ -0,0 +1,48 @@
+package com.example.project_spring_boot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@ManyToOne
+    //User
+
+    String name;
+
+    String description;
+
+    public Item() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
---
+++ src/main/java/com/example/project_spring_boot/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.example.project_spring_boot.service;
+
+import java.util.List;
+
+import com.example.project_spring_boot.entity.Item;
+
+public interface ItemService {
+    Item getItem(Long id);
+    Item createItem(Item item);
+
+    List<Item> getItems();
+}
