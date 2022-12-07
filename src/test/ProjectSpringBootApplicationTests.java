fc14b91d48cb1715d44739529e2134e507fe95e1
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-07 13:20:27 +0100

    remove test class

---
+++ src/test/ProjectSpringBootApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.project_spring_boot;
+
+/*import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+} */
