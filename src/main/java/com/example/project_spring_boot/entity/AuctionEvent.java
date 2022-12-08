02beb90327a43ee770f5ab4d2e210ea58813968b
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-08 20:29:36 +0100

    implement AuctionEvent Entity

---
+++ src/main/java/com/example/project_spring_boot/entity/AuctionEvent.java
@@ -0,0 +1,59 @@
+package com.example.project_spring_boot.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+public class AuctionEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+    private LocalDateTime startTime;
+    private LocalDateTime stopTime;
+    private Boolean status; 
+
+    @ManyToOne
+    private Item item;
+
+    // @ManyToOne
+    // private User user; // add Getters + Setters if needed 
+
+    public AuctionEvent() {  // Add params later 
+       
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(LocalDateTime stopTime) {
+        this.stopTime = stopTime;
+    } 
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+    
+}
