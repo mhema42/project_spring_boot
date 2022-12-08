57b33c7cf2a62f5d14c9c2bd242ccf0443fe462f
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-08 20:41:46 +0100

    Add service files for AuctionEvent

---
+++ src/main/java/com/example/project_spring_boot/service/AuctionEventImpl.java
@@ -0,0 +1,18 @@
+package com.example.project_spring_boot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.project_spring_boot.repository.AuctionEventRepository;
+
+public class AuctionEventImpl implements AuctionService {
+
+    @Autowired
+    AuctionEventRepository auctionEventRepository;
+
+    // getAuctionEvent
+
+    // getAuctionEvents
+
+    // newAuctionEvent
+    
+}
