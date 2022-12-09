7ad1876d97482e1f524451c21d6683e1f7c688dd
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-09 10:36:11 +0100

    Add imports

--- src/main/java/com/example/project_spring_boot/controller/ItemController.java
+++ src/main/java/com/example/project_spring_boot/controller/ItemController.java
@@ -1,8 +1,17 @@
 package com.example.project_spring_boot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.project_spring_boot.entity.Item;
 import com.example.project_spring_boot.service.ItemService;
 
 @RestController
