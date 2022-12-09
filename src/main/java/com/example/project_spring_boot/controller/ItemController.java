a74706ed340f8f73248a40f77a70567533ab9414
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-09 11:31:20 +0100

    minor fix

--- src/main/java/com/example/project_spring_boot/controller/ItemController.java
+++ src/main/java/com/example/project_spring_boot/controller/ItemController.java
@@ -10,12 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.project_spring_boot.entity.Item;
 import com.example.project_spring_boot.service.ItemService;
@@ -36,7 +30,7 @@ public class ItemController {
     }
 
      @PostMapping("/item")
-     public ResponseEntity<Item> saveCar(@RequestBody Item item) {
+     public ResponseEntity<Item> saveItem(@RequestBody Item item) {
         return new ResponseEntity<>(itemService.createItem(item), HttpStatus.CREATED);
      }
     
