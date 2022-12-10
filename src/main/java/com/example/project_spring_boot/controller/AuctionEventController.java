c35931dd0f95a5d5d36e0b2bc8eee03ad01d9173
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-10 21:07:37 +0100

    Implement request methods for AuctionEvent

--- src/main/java/com/example/project_spring_boot/controller/AuctionEventController.java
+++ src/main/java/com/example/project_spring_boot/controller/AuctionEventController.java
@@ -1,8 +1,15 @@
 package com.example.project_spring_boot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.project_spring_boot.entity.AuctionEvent;
 import com.example.project_spring_boot.service.AuctionEventService;
 
 @RestController
@@ -11,4 +18,14 @@ public class AuctionEventController {
     @Autowired
     AuctionEventService auctionEventService;
     
+    @GetMapping("/auctionevent/{id}")
+    public ResponseEntity<AuctionEvent> getAuctionEvent(@PathVariable Long id) {
+        return new ResponseEntity<>(auctionEventService.getAuctionEvent(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/auctionevent")
+    public ResponseEntity <List<AuctionEvent>> getAuctionEvents() {
+        return new ResponseEntity<>(auctionEventService.getAuctionEvents(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/project_spring_boot/service/AuctionEventServiceImpl.java
+++ src/main/java/com/example/project_spring_boot/service/AuctionEventServiceImpl.java
@@ -1,19 +1,31 @@
 package com.example.project_spring_boot.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.project_spring_boot.entity.AuctionEvent;
 import com.example.project_spring_boot.repository.AuctionEventRepository;
 
 @Service
 public class AuctionEventServiceImpl implements AuctionEventService {
 
     @Autowired
-    AuctionEventRepository auctionEventRepository;
+    AuctionEventRepository auctionEventRepository; 
 
-    // getAuctionEvent
+    @Override
+    public AuctionEvent getAuctionEvent(Long id) {
+        return auctionEventRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request"));
+    }
 
-    // getAuctionEvents
+    @Override
+    public List<AuctionEvent> getAuctionEvents() {
+        return (List<AuctionEvent>)auctionEventRepository.findAll();
+    }
 
     // newAuctionEvent
     
