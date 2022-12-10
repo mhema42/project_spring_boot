763f1df3c6f4c725ad155f045f241dbbf11abb1f
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-10 20:19:24 +0100

    Add getters and setters for id

--- src/main/java/com/example/project_spring_boot/entity/AuctionEvent.java
+++ src/main/java/com/example/project_spring_boot/entity/AuctionEvent.java
@@ -24,6 +24,14 @@ public class AuctionEvent {
        
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public LocalDateTime getStartTime() {
         return startTime;
     }
