b302192e97e95d60e06a8eb20fd2aa39fe0bb260
Mats <mhema@telia.com>
2022-12-11 21:53:14 +0100

    add endpoints and implementation for users post, getOne, getAll

--- src/main/java/com/example/project_spring_boot/controller/UserController.java
+++ src/main/java/com/example/project_spring_boot/controller/UserController.java
@@ -1,8 +1,17 @@
 package com.example.project_spring_boot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.project_spring_boot.entity.User;
 import com.example.project_spring_boot.service.UserService;
 
 @RestController
@@ -10,4 +19,18 @@ public class UserController {
     @Autowired 
     UserService userService;
     
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity <List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+     @PostMapping("/user")
+     public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+     }
 }
--- src/main/java/com/example/project_spring_boot/repository/ItemRepository.java
+++ src/main/java/com/example/project_spring_boot/repository/ItemRepository.java
@@ -1,11 +1,6 @@
 package com.example.project_spring_boot.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
 import com.example.project_spring_boot.entity.Item;
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
