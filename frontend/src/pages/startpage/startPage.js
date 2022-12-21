9dd8a6e6abee40789dbdc80ef3742e800efb9225
Mats <mhema@telia.com>
2022-12-21 11:22:26 +0100

    renders auctionevent and bid on startpage, begun implementing filter autionevent on active and highest bid

--- frontend/src/pages/startpage/startPage.js
+++ frontend/src/pages/startpage/startPage.js
@@ -4,7 +4,8 @@ import ButtonAppBar from "../../components/ButtonAppBar";
 
 
 const StartPage = () => {
-    const [items, setItems] = useState([])
+    const [auctions, setAuctions] = useState([])
+    const [bids, setBids] = useState([])
 
     var divStyle = {
         background: "#eee",
@@ -13,7 +14,7 @@ const StartPage = () => {
     };
 
     useEffect(() => {
-        fetch("http://localhost:8080/item", {
+        fetch("http://localhost:8080/bid", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -21,7 +22,7 @@ const StartPage = () => {
         })
             .then(res => res.json())
             .then((result) => {
-                setItems(result);
+                setBids(result);
             }
             )
     }, [])
@@ -31,15 +32,17 @@ const StartPage = () => {
         <div>
             <ButtonAppBar />
             <h1>
-                This is the startpage
+                This is the startpage for auctions
             </h1>
-            <h1>Items</h1>
             <Paper elevation={6}>
 
-                {items.map(item => (
-                    <Paper key={item.id} style={divStyle}>
-                        name: {item.name} <br />
-                        description: {item.description} <br />
+                {bids.map(bids => (
+                    <Paper key={bids.id} style={divStyle}>
+                        AuctionId: {bids.auctionEvent.id} <br />
+                        Item: {bids.auctionEvent.item.name} <br />
+                        Description: {bids.auctionEvent.item.description} <br />
+                        Stoptime: {bids.auctionEvent.stopTime} <br />
+                        Bid: {bids.offer} <br />
                     </Paper>
                 ))
                 }
