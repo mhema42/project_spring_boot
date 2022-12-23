11f68d5dad0fa7965b5b599c51f8fccf1baecd30
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-23 04:16:37 +0100

    Add page to view my auctionEvents

--- frontend/src/pages/myPage/myAuctionPage/myAuctionPage.js
+++ frontend/src/pages/myPage/myAuctionPage/myAuctionPage.js
@@ -4,16 +4,23 @@ import ButtonAppBar from "../../../components/ButtonAppBar";
 
 const ActiveAuctionPage = () => {
     const [AuctionEvents, setAuctionEvents] = useState([]);
-    const [userId, setUserId] = useState(1);
+    const [status, setStatus] = useState(false);
+    const [btnText, setBtnText] = useState("View endend auctions");
+    const [title, setTitle] = useState("My active auctions");
+    const [userId] = useState(() => {
+        const saved = localStorage.getItem("userToken");
+        const initialValue = JSON.parse(saved);
+        return initialValue || "";
+    });
 
     let divStyle = {
         background: "#eee",
         padding: "20px",
-        margin: "20px"
+        margin: "20px"  
     };
 
     useEffect(() => {
-        fetch("http://localhost:8080/auctionevent/user/"+ userId +"/active/true", {
+        fetch("http://localhost:8080/auctionevent/user/"+ userId + "/active/true", {  
             method: "GET",
         })
         .then(res => res.json())
@@ -23,23 +30,47 @@ const ActiveAuctionPage = () => {
         })
     }, [])
 
+    async function HandleStatus() { 
+        const res = await fetch(`http://localhost:8080/auctionevent/user/${userId}/active/${status}`,
+        {
+            method: "get",
+        })  
+        const result = await res.json();  
+        setAuctionEvents(result);
+        setStatus(current => !current);
+        setBtnText(current => !current);
+        setTitle(current => !current);
+        console.log(status);
+    }
+
     return (
         <div>
             <ButtonAppBar />
             <div className="container">
                 <h1>
-                    My active auctions
+                    {title? 'My active auctions' : 'My ended auctions'} 
                 </h1>
+
+                <div>
+                    <button value={status? false : true} onClick={(ev) => HandleStatus(ev.target.value)}> 
+                        {btnText? 'View ended auctions' : 'View active auctions'} 
+                    </button>
+                </div>
+
                 <Paper elevation={6}> 
                     {AuctionEvents.map((auctionEvent) => {
                         return (
                             <Paper key={auctionEvent.id} style={divStyle}>
                                 {auctionEvent.item.name}
                                 <br /> 
+                                Auction end: {auctionEvent.stopTime} 
+                                <br />
+                                <div className="image-container">
+                                    <img className="image" alt={"upploaded by the user"} src={`data:image/png;base64,${auctionEvent.item.image}`} />
+                                </div>
                                 {auctionEvent.item.description}
                                 <br />
-                                Auction ends: {auctionEvent.stopTime} 
-                                <br /> 
+                                
                                 {auctionEvent.bids.map((bid) => {
                                     return (
                                         <Paper key={bid.id} style={divStyle}>
