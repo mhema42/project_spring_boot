c7842878d2525e388b8e47b7222370abb7f4d461
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-27 14:12:31 +0100

    Add date and number format

--- frontend/src/pages/myPage/myAuctionPage/myAuctionPage.js
+++ frontend/src/pages/myPage/myAuctionPage/myAuctionPage.js
@@ -54,7 +54,8 @@ const ActiveAuctionPage = () => {
                         <div className="auction-content" key={auctionEvent.id}>
                             <div className="partial-1">
                                 <span className="name"> {auctionEvent.item.name} </span> 
-                                <span> Auction ends: {auctionEvent.stopTime} </span>
+                                <span> Top bid: ${Intl.NumberFormat('eur').format(auctionEvent.highestBid)} </span>
+                                <span> Ends: {auctionEvent.stopTime.toString().substring(2, 10) + " kl." + auctionEvent.stopTime.toString().substring(11, 16)} </span>
                             </div>
                             <div className="partial-2">
                                 <div>
@@ -68,7 +69,8 @@ const ActiveAuctionPage = () => {
                                 return (
                                     <div className="bid-content" key={bid.id}>
                                         <span className="bidder"> Bidder: {bid.bidder.username} </span>
-                                        <span className="bid"> Bid: {bid.offer} </span>  
+                                        <span className="bidder"> {bid.bidTime.toString().substring(2, 10) + " kl." + bid.bidTime.toString().substring(11, 16)} </span>
+                                        <span className="bid"> ${Intl.NumberFormat('eur').format(bid.offer)}</span>  
                                     </div>
                                 );
                             })}  
