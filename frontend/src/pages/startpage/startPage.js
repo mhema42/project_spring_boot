3ab7d3037f1e09d4874add7e675a450f7e9b11a1
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-27 14:13:18 +0100

    Add modal to startPage

--- frontend/src/pages/startpage/startPage.js
+++ frontend/src/pages/startpage/startPage.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import ButtonAppBar from "../../components/ButtonAppBar";
 import axios from 'axios';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 import "../../css/startPage.css";
 
 const StartPage = () => {
@@ -13,16 +18,21 @@ const StartPage = () => {
         const initialValue = JSON.parse(saved);
         return initialValue || "";
     });
-    const [isDisabled, setIsDisabled] = useState(true);
-    const [checked, setChecked] = useState(false);
 
-    const canBeSubmitted = () => {
-        return checked ? setIsDisabled(true) : setIsDisabled(false);
-    };
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    const onCheckboxClick = () => {
-        setChecked(!checked);
-        return canBeSubmitted();
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
     };
 
     useEffect(() => {
@@ -39,11 +49,12 @@ const StartPage = () => {
             )
     }, [])
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+
         const offer = { "offer": bid}
         console.log(offer)
-        fetch(`http://localhost:8080/bid?auctioneventId=${auctionEventId}&bidderId=${userId}`, {
+        await fetch(`http://localhost:8080/bid?auctioneventId=${auctionEventId}&bidderId=${userId}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -63,6 +74,27 @@ const StartPage = () => {
                     All active auctions
                 </h1>
 
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                    >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Add you bid
+                    </Typography>
+                    <br />
+
+                    <form onSubmit={handleSubmit}>
+                        <input name="bid" type="text" value={bid} onChange={(e) => setBid(e.target.value)} required="required"/>
+            
+                        <button type="submit"> Submitt bid </button>
+                    </form>
+
+                </Box>
+                </Modal>
+
                 {auctions.map(auction => (
                     <div className="auction-content" key={auction.id}>
             
@@ -77,16 +109,13 @@ const StartPage = () => {
                                 <img className="image" alt={"upploaded by the user"} src={`data:image/png;base64,${auction.item.image}`} />
                             </div>
 
-                            <form onSubmit={handleSubmit}>
-                                <input name="bid" type="text" value={bid} onChange={(e) => setBid(e.target.value)} required="required"/>
+                      
 
-                                <label>
-                                    <input type="radio"  value={auction.id} onChange={(e) => setAuctionEventId(e.target.value)} required="required" onClick={onCheckboxClick}/>
-                                    Confirm your bid!
-                                </label>
-            
-                                <button disabled={isDisabled} type="submit"> Submitt bid </button>
-                            </form>
+                            <Button value={auction.id} onClick={(e) => {    
+                                setAuctionEventId(e.target.value);
+                                handleOpen()}}> 
+                                    Add your bid
+                            </Button>
 
                             <span className="description"> {auction.item.description} </span>
                         </div>
